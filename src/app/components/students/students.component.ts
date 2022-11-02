8d375f3719c8e041de2a8293f9705c143e3e1cd7
srinivas96660 <enjamsrinivas9393@gmail.com>
2022-11-02 16:14:05 +0530

    form implemented persnol details

--- src/app/components/students/students.component.ts
+++ src/app/components/students/students.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { max } from 'rxjs';
 
 @Component({
   selector: 'app-students',
@@ -6,6 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./students.component.css']
 })
 export class StudentsComponent implements OnInit {
+  public studentForm:FormGroup=new FormGroup(
+    {
+      name:new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(10)]),
+      mobile:new FormControl(null, [Validators.required, Validators.min(1000000000), Validators.max(9999999999)]),
+      email:new FormControl(null, [Validators.required]),
+      batch:new FormControl(null, [Validators.required]),
+
+
+    }
+  )
 
   constructor() { }
 
